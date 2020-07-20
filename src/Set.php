e431b7c3765a60c5a37c6144dfbf657fd3ad8880
Raymond Chong <mathsgod@yahoo.com>
2020-07-20 10:59:20 +0800

    init

--- src/Set.php
+++ src/Set.php
@@ -4,4 +4,12 @@ namespace PHP\Util;
 
 class Set extends Collection
 {
+
+    public function add($element)
+    {
+        //check duplicate
+        if (!$this->contains($element)) {
+            parent::add($element);
+        }
+    }
 }
