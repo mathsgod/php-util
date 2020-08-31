2a3c438b9d74a5a4d403f003bab419139fbbd52a
Raymond Chong <mathsgod@yahoo.com>
2020-08-31 17:16:03 +0800

    update

--- src/QueryInterface.php
+++ src/QueryInterface.php
@@ -7,5 +7,10 @@ interface QueryInterface
     public function limit(int $limit);
     public function offset(int $offset);
     public function filter(array $filter);
-    public function orderBy(array $order);
+
+    /**
+     * @param array|string $order
+     * suggest use array
+     */
+    public function orderBy($order);
 }
